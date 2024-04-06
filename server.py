5e8725214e9abb17cd8515d76015b322edd1b931
kkuang <kkuang@Ubuntu.kkuang.virtualbox.org>
2024-04-06 00:11:09 -0700

    simple websocket messaging

---
+++ server.py
@@ -0,0 +1,21 @@
+import asyncio
+import websockets
+
+connected = set()
+
+async def server(websocket, path):
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            print(message)
+            for conn in connected:
+                if conn != websocket:
+                    await conn.send(message)
+    finally:
+        connected.remove(websocket)
+
+start_server = websockets.serve(server, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+            
